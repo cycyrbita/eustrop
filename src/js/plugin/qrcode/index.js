27bfa7eeda3795e71ea3c6d41f18638679b161d6
Nikita job <cycyrbita@mail.ru>
2021-09-10 12:59:27 +0300

    правки

--- src/js/plugin/qrcode/index.js
+++ src/js/plugin/qrcode/index.js
@@ -2295,3 +2295,30 @@ var qrcode = function() {
 }(function () {
     return qrcode;
 }));
+
+//---------------------------------------------------------------------
+//
+// QR Code Generator for JavaScript UTF8 Support (optional)
+//
+// Copyright (c) 2011 Kazuhiko Arase
+//
+// URL: http://www.d-project.com/
+//
+// Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+// The word 'QR Code' is registered trademark of
+// DENSO WAVE INCORPORATED
+//  http://www.denso-wave.com/qrcode/faqpatent-e.html
+//
+//---------------------------------------------------------------------
+
+!function(qrcode) {
+
+  //---------------------------------------------------------------------
+  // overwrite qrcode.stringToBytes
+  //---------------------------------------------------------------------
+
+  qrcode.stringToBytes = qrcode.stringToBytesFuncs['UTF-8'];
+
+}(qrcode);
