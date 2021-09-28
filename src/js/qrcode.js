162d64ca69754dfe6befac232b603702dd818c25
Nikita <cycyrbita@mail.ru>
2021-09-28 22:49:19 +0300

    правки

--- src/js/qrcode.js
+++ src/js/qrcode.js
@@ -1,11 +1,16 @@
 window.onload = function () {
     activeNav();
     activeType();
+    let infoForm = [];
+    dataForm('qrcode', infoForm);
+    
 
     // делаем активными пункты меню
     function activeNav() {
         let elem = document.querySelectorAll('.qrcode__nav label input');
         let label = document.querySelectorAll('.qrcode__nav label');
+        let indexField = document.querySelectorAll('.qrcode__body > div');
+        
 
         for(let i = 0; i < elem.length; i++) {
             elem[i].addEventListener('change', function() {
@@ -14,7 +19,13 @@ window.onload = function () {
                         label[k].classList.remove('active');
                     }
                 }
+                for(let j = 0; j < indexField.length; j++) {
+                    if(indexField[j].classList.contains('visible')) {
+                        indexField[j].classList.remove('visible');
+                    }
+                }
                 elem[i].closest('label').classList.add('active');
+                indexField[i].classList.add('visible');
             });
         }
     }
@@ -33,6 +44,32 @@ window.onload = function () {
                 }
                 elem[i].closest('label').classList.add('active');
             });
+        }
+    }
+
+    // получаем данные с формы
+    function dataForm(idForm, arr) {
+        // кладем форму в переменную
+        idForm = document.getElementById(idForm);
+
+        // проверка на наличие формы
+        if(idForm) {
+            // событие отправки формы
+            idForm.onsubmit = function(e) {
+                // отменяем отправку формы
+                e.preventDefault();
+
+                // собираем информацию с формы
+                let formData = new FormData(idForm);
+
+                // пробегаемся по ключам
+                for (var key of formData.entries()) {
+                    // если значение ключа не пустое то пушим его в массив
+                    if(key[1] != '') {
+                        arr.push(key);
+                    }
+                }
+            }
         }
     }
 }
