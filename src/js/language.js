790503f8aa99e9a0d9726033081fe7d9b9b09ca1
Nikita <cycyrbita@mail.ru>
2021-12-04 02:04:29 +0300

    правки

--- src/js/language.js
+++ src/js/language.js
@@ -1,63 +1,179 @@
-
 window.addEventListener("load", function () {
     var language = {
-        "RU": [
-            {
-                "QR-code": "QR-code",
-                "Participants": "Участники",
-                "QR-Generator": "QR-Генератор",
-                "Ranges": "Диапазоны",
-                "Map": "Карта",
-                "Help": "Помощь",
-                "Registration": "Регистрация",
-                "Specifications": "Характеристики",
-                "Service": "Обслуживание",
-                "Open registration form": "Открыть форму регистрации",
-                "Qxyz home page": "Главная страница qxyz",
-                "entrance": "Вход",
-                "Squares": "Квадратики",
-                "Lang": "Язык",
-                "qrcode": "qrcode"
-            }
-        ],
-        "EN": [
-            {
-                "QR-code": "QR-code",
-                "Участники": "Participants",
-                "QR-Генератор": "QR-Generator",
-                "Диапазоны": "Ranges",
-                "Карта": "Map",
-                "Помощь": "Help",
-                "Регистрация": "Registration",
-                "Характеристики": "Specifications",
-                "Обслуживание": "Service",
-                "Открыть форму регистрации": "Open registration form",
-                "Главная страница qxyz": "Qxyz home page",
-                "Вход": "entrance",
-                "Квадратики": "Squares",
-                "Язык": "Lang",
-                "qrcode": "qrcode"
-            }
-        ],
-        "ES": [
-            {
-                "QR-code": "QR-code",
-                "Участники": "Participantes",
-                "QR-Генератор": "Generador de QR",
-                "Диапазоны": "Rangos",
-                "Карта": "Mapa",
-                "Помощь": "Ayudar",
-                "Регистрация": "registro",
-                "Характеристики": "Especificaciones",
-                "Обслуживание": "Servicio",
-                "Открыть форму регистрации": "Formulario de registro abierto",
-                "Главная страница qxyz": "Página de inicio de Qxyz",
-                "Вход": "Entrada",
-                "Квадратики": "Cuadrícula",
-                "Язык": "Idioma",
-                "qrcode": "qrcode"
-            }
-        ]
+        "RU": [{
+            "lt1": "qxyz",
+            "lt2": "QR-code",
+            "lt3": "QR-Генератор",
+            "lt4": "Диапазоны",
+            "lt5": "Карта",
+            "lt6": "Help!",
+            "lt7": "Участники",
+            "lt8": "Участники",
+            "lt9": "QR-Генератор",
+            "lt10": "Диапазоны",
+            "lt11": "Карта",
+            "lt12": "lorem",
+            "lt13": "QR-Генератор",
+            "lt14": "Диапазоны",
+            "lt15": "Карта",
+            "lt16": "Help!",
+            "lt17": "Help!",
+            "lt18": "Участники",
+            "lt19": "Участники",
+            "lt20": "QR-Генератор",
+            "lt21": "Диапазоны",
+            "lt22": "Карта",
+            "lt23": "Help!",
+            "lt24": "QR-Генератор",
+            "lt25": "Диапазоны",
+            "lt26": "Карта",
+            "lt27": "Help!",
+            "lt28": "Регистрация",
+            "lt29": "RU",
+            "lt30": "EN",
+            "lt31": "Характеристики",
+            "lt32": "Обслуживание",
+            "lt33": "Открыть форму регистрации",
+            "lt34": "Главная страница qxyz",
+            "lt35": "Вход",
+            "lt36": "Квадратики",
+            "lt37": "qrcode",
+            "lt38": "Регистрация",
+            "lt39": "Введите ваше имя",
+            "lt40": "Введите корректное имя...",
+            "lt41": "Введите вашу почту",
+            "lt42": "Такой почты не существует...",
+            "lt43": "Форма участия",
+            "lt44": "Очно",
+            "lt45": "Очно, прочитаю доклад",
+            "lt46": "Дистанционно (трансляцию)",
+            "lt47": "Дистанционно и очно",
+            "lt48": " Посмотрю запись после",
+            "lt49": "Выберите форму участия...",
+            "lt50": "Напишите нам",
+            "lt51": "Выберите форму участия...",
+            "lt52": "Я согласен с условиями обработки персональных данных",
+            "lt53": "Выберите форму участия...",
+            "lt54": "Зарегистрироваться",
+            "lt55": "Войти",
+            "lt56": "Copyright ©"
+        }],
+        "EN": [{
+            "lt1": "qxyz",
+            "lt2": "QR-code",
+            "lt3": "QR-Генератор",
+            "lt4": "Диапазоны",
+            "lt5": "Карта",
+            "lt6": "Help!",
+            "lt7": "Участники",
+            "lt8": "Участники",
+            "lt9": "QR-Генератор",
+            "lt10": "Диапазоны",
+            "lt11": "Карта",
+            "lt12": "lorem",
+            "lt13": "QR-Генератор",
+            "lt14": "Диапазоны",
+            "lt15": "Карта",
+            "lt16": "Help!",
+            "lt17": "Help!",
+            "lt18": "Участники",
+            "lt19": "Участники",
+            "lt20": "QR-Генератор",
+            "lt21": "Диапазоны",
+            "lt22": "Карта",
+            "lt23": "Help!",
+            "lt24": "QR-Генератор",
+            "lt25": "Диапазоны",
+            "lt26": "Карта",
+            "lt27": "Help!",
+            "lt28": "Регистрация",
+            "lt29": "RU",
+            "lt30": "EN",
+            "lt31": "Характеристики",
+            "lt32": "Обслуживание",
+            "lt33": "Открыть форму регистрации",
+            "lt34": "Главная страница qxyz",
+            "lt35": "Вход",
+            "lt36": "Квадратики",
+            "lt37": "qrcode",
+            "lt38": "Регистрация",
+            "lt39": "Введите ваше имя",
+            "lt40": "Введите корректное имя...",
+            "lt41": "Введите вашу почту",
+            "lt42": "Такой почты не существует...",
+            "lt43": "Форма участия",
+            "lt44": "Очно",
+            "lt45": "Очно, прочитаю доклад",
+            "lt46": "Дистанционно (трансляцию)",
+            "lt47": "Дистанционно и очно",
+            "lt48": " Посмотрю запись после",
+            "lt49": "Выберите форму участия...",
+            "lt50": "Напишите нам",
+            "lt51": "Выберите форму участия...",
+            "lt52": "Я согласен с условиями обработки персональных данных",
+            "lt53": "Выберите форму участия...",
+            "lt54": "Зарегистрироваться",
+            "lt55": "Войти",
+            "lt56": "Copyright ©"
+        }],
+        "ES": [{
+            "lt1": "qxyz",
+            "lt2": "dfgdfgcvb",
+            "lt3": "dfgfdger",
+            "lt4": "uiyyhjkhjk",
+            "lt5": "hmbnmbn",
+            "lt6": "ghngbtrt!",
+            "lt7": "kj,jyhk,ghj",
+            "lt8": "vbnvb",
+            "lt9": "hrtyukuti",
+            "lt10": "edffdbcv",
+            "lt11": ";;ikkkk",
+            "lt12": "lorem",
+            "lt13": "QR-Генератор",
+            "lt14": "ui,k,j,",
+            "lt15": "sdfsdf",
+            "lt16": "sxcxcvs!",
+            "lt17": "mbmmghjg!",
+            "lt18": "Учаrfgfdghстники",
+            "lt19": "zfbvfxbnfgn",
+            "lt20": "QR-Генератор",
+            "lt21": "vbcvccccc",
+            "lt22": "cccvbcvb",
+            "lt23": "Help!",
+            "lt24": "fgnvgntg",
+            "lt25": "tyjyhjkhj",
+            "lt26": "hjkghn",
+            "lt27": "Help!",
+            "lt28": "oioyiioyiy",
+            "lt29": "dfgfdg",
+            "lt30": "fghfgh",
+            "lt31": "fghjgjkl",
+            "lt32": "jeteyg",
+            "lt33": "m,.nmnm",
+            "lt34": "ewsfvb",
+            "lt35": "fgdhsvbc",
+            "lt36": "fngmhgj,mhjg,",
+            "lt37": "qrcode",
+            "lt38": "cxvbcxvbsfb",
+            "lt39": "dfgcvbdsb",
+            "lt40": "fjrttrrt",
+            "lt41": "Введите вашу почту",
+            "lt42": "fghrghfdn",
+            "lt43": "fghfdghfhfdgn",
+            "lt44": "sdfcvcxve",
+            "lt45": "fdcvbed",
+            "lt46": "dfgvbcv",
+            "lt47": "fghhjtyt",
+            "lt48": "uiluilhg",
+            "lt49": "dfcveegegr",
+            "lt50": "fgfhffffff",
+            "lt51": "fghykfhggh",
+            "lt52": "sdfs",
+            "lt53": "werfsdfd",
+            "lt54": "sdfssdf",
+            "lt55": "hsdf sfw",
+            "lt56": "Copyright ©"
+        }]
     }
 
     lang("*", 'lt', true);
@@ -68,22 +184,22 @@ window.addEventListener("load", function () {
 
         // тут будет лежать весь текст который найдем
         var pageContent = {};
-        
+
         // класс который будем добавлять всем текстам у которых есть текст
         var lt = className;
 
         // префикс для класса
         var count = 0;
 
-        for(var i = 0; i < tagName.length; i++) {
+        for (var i = 0; i < tagName.length; i++) {
             // исключаем теги
-            if(tagName[i].tagName != 'SCRIPT' && tagName[i].tagName != 'STYLE') {
+            if (tagName[i].tagName != 'SCRIPT' && tagName[i].tagName != 'STYLE') {
                 // проверяем есть ли текст
-                if(tagName[i].innerHTML) {
+                if (tagName[i].innerHTML) {
                     // узнаем есть ли текст внутри элемента(все дочерние теги отсеиваем)
-                    for(var k = 0; k < tagName[i].childNodes.length; k++) {
+                    for (var k = 0; k < tagName[i].childNodes.length; k++) {
                         // узнаем что содержится внутри тега(текст или тег)
-                        if(tagName[i].childNodes[k].nodeType === 3 && tagName[i].childNodes[k].nodeValue != 0) {
+                        if (tagName[i].childNodes[k].nodeType === 3 && tagName[i].childNodes[k].nodeValue != 0) {
                             // чистим классс
                             lt = className;
 
@@ -102,7 +218,7 @@ window.addEventListener("load", function () {
         }
 
         // выводим содержимое в консоль
-        if(log) {
+        if (log) {
             console.log(pageContent);
         }
     }
